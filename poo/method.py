3a7c49105ca0eaa43535a0606c36b8dc9da77fc0
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-17 18:25:35 -0300

    studying methods

---
+++ poo/method.py
@@ -0,0 +1,22 @@
+class Person:
+    actual_year = 2022
+
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+    def birth_year(self):
+        return self.actual_year-self.age
+    
+    @classmethod 
+    def birth_year(cls,name,birth_year):
+        idade = cls.actual_year-birth_year
+        return cls(name,birth_year)
+
+#To define a method utilize a @, 
+#it isn't related to the instanse, but to the class itself
+#cls it's a convention utilizated to refer to the class 
+
+p1 = Person.birth_year('Name',2003)
+print(p1)
+print(p1.name,p1.age)
