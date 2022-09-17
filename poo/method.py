6a126dae2435ce15674ec8623e3eed764f49ac83
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-17 18:34:50 -0300

    add static method

--- poo/method.py
+++ poo/method.py
@@ -1,3 +1,4 @@
+from random import randint
 class Person:
     actual_year = 2022
 
@@ -13,10 +14,18 @@ class Person:
         idade = cls.actual_year-birth_year
         return cls(name,birth_year)
 
-#To define a method utilize a @, 
+#To define a method utilize a @
 #it isn't related to the instanse, but to the class itself
 #cls it's a convention utilizated to refer to the class 
+
+    @staticmethod
+    def make_id():
+        return randint(1000,1999)
+
+#The static method dosn't need any instance or class
+
 
 p1 = Person.birth_year('Name',2003)
 print(p1)
 print(p1.name,p1.age)
+print(p1.make_id())
